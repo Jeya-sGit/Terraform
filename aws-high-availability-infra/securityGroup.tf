019ab4597afd5c8fa305860c4da15a1084851cf3
JEYA NAGARAJA J <jjeyanagaraja8602@gmail.com>
2025-11-27 22:45:51 +0530

    Added new SG

--- aws-high-availability-infra/securityGroup.tf
+++ aws-high-availability-infra/securityGroup.tf
@@ -1,9 +1,69 @@
+#1.Security Group for ALB(Internet -> ALB)
+resource "aws_security_group" "alb_sg " {
+  name        = "alb-sg"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = aws_vpc.VPCDemoTerraform.id
+
+    ingress {
+        description = "HTTP from anywhere"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        description = "All outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name="terraform_alb_sg"
+    }
+
+}
+
+#2.Security Group for EC2 instances behind ALB (ALB -> EC2)
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Security group for EC2 instances behind ALB"
+  vpc_id      = aws_vpc.VPCDemoTerraform.id
+
+    ingress {
+        description       = "HTTP from ALB"
+        from_port         = 80
+        to_port           = 80
+        protocol          = "-1"
+        security_groups   = [aws_security_group.alb_sg.id]
+    }
+    egress {
+        description = "All outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name="terraform_ec2_sg"
+    }    
+} 
+
 #Provision a security group for public EC2 instances
 resource "aws_security_group" "public_ec2_sg"{
     name="public-ec2-sg"
     description="Security group for public EC2 instances"
     vpc_id=aws_vpc.VPCDemoTerraform.id
 
+    ingress {
+        description = "SSH from my IP"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["157.51.37.120/32"]
+    }
     ingress {
         description = "SSH from my IP"
         from_port   = 22
@@ -33,7 +93,7 @@ resource "aws_security_group" "public_ec2_sg"{
         cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
-        Name = "public-ec2-sg"
+        Name = "public_ec2_sg"
     }
 }    
 
@@ -60,6 +120,6 @@ resource "aws_security_group" "private_ec2_sg"{
   }
 
   tags = {
-    Name = "private-ec2-sg"
+    Name = "private_ec2_sg"
   }
 }
